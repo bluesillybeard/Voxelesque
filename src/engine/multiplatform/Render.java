27dc3be9ea99a12eb306f473ed2571552448773c
bluesillybeard <bluesillybeard2@gmail.com>
2021-10-03 23:13:01 -0600

    work on rewriting

--- src/engine/multiplatform/Render.java
+++ src/engine/multiplatform/Render.java
@@ -9,7 +9,7 @@ public interface Render {
      * Initializes the Render and anything contained within it.
      * @param width the width of the window (800 if given an invalid width)
      * @param height the height of the window (600 if given an invalid height)
-     * @param resourcesPath The path to the resources folder. This path is added at the front of any path given o the Render.
+     * @param resourcesPath The path to the resources folder. This path is added at the front of any path to be loaded by the Render.
      * @param VSync Vsync
      * @param warning the warning PrintStream, Any warning will be sent through it.
      * @param error the error PrintStream. Any errors will be sent through it.
