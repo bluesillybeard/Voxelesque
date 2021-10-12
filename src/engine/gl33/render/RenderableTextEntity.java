87a8fc2fa6f824266edf7d2f6e34d77238929261
bluesillybeard <bluesillybeard2@gmail.com>
2021-10-12 12:00:06 -0600

    text rendering

--- src/engine/gl33/render/RenderableTextEntity.java
+++ src/engine/gl33/render/RenderableTextEntity.java
@@ -3,14 +3,19 @@ package engine.gl33.render;
 import engine.gl33.model.GPUMesh;
 import engine.gl33.model.GPUModel;
 import engine.gl33.model.GPUTexture;
+import engine.multiplatform.Util.Utils;
 
 public class RenderableTextEntity extends RenderableEntity{
-    //TODO: you started on text rendering.
-    public RenderableTextEntity(GPUMesh mesh, ShaderProgram shaderProgram, GPUTexture texture) {
-        super(mesh, shaderProgram, texture);
+    //this class is VERY small, but that's because it doesn't add a lot of functionality.
+    private String text;
+    public RenderableTextEntity(String text, ShaderProgram shader, GPUTexture tex, boolean centerX, boolean centerY) {
+        super(new GPUMesh(Utils.generateTextMesh(text, centerX, centerY)), shader, tex);
+        this.text = text;
     }
-
-    public RenderableTextEntity(GPUModel model, ShaderProgram shaderProgram) {
-        super(model, shaderProgram);
+    public void setText(String text, boolean centerX, boolean centerY){
+        if(!this.text.equals(text)){
+            this.text = text;
+            super.setModel(new GPUModel(new GPUMesh(Utils.generateTextMesh(text, centerX, centerY)), super.getModel().texture));
+        }
     }
 }
--- src/engine/multiplatform/Util/Utils.java
+++ src/engine/multiplatform/Util/Utils.java
@@ -1,7 +1,10 @@
 package engine.multiplatform.Util;
 
+import engine.multiplatform.model.CPUMesh;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class Utils {
     private static final double HALF_PI = Math.PI/2.;
@@ -11,17 +14,87 @@ public class Utils {
         return(Files.readString(Path.of(fileName)));
     }
 
-    public static double fastSine(double t){
-        //A faster sine calculation based on polynomials and modulus.
-        //about twice as fast as using Math.sin()
+    public static CPUMesh generateTextMesh(String text, boolean centerX, boolean centerY){
+        //Yes, I am aware that this is a really inefficient way of doing it.
+        //But, unless we are rendering insane amounts of text, I think it will be fine.
 
-        //uses a modulus to truncate to a repeating pattern between x=0 and x=2pi
-        //then puts that into a 4th degree polynomial to approximate a sine function.
-        //ok-ish for graphics and audio, VERY BAD for anything that requires any accuracy at all.
-        // may be off by as much as 0.15, most accurate near highest and lowest outputs
-        // could use lots more tweaking, this is more of a proof of concept
-        double x = Math.abs(t-HALF_PI % DOUBLE_PI);
-        return -0.005 * ((x-Math.PI)*(x-Math.PI)*(2*x-4.847*Math.PI)*(2*x+0.847*Math.PI))-1;
+        char[] iterableText = text.toCharArray();
+        ArrayList<ArrayList<Character>> string = new ArrayList<>();
+        ArrayList<Character> line = new ArrayList<>();
+        int numCharacters = 0;
+        for(char character: iterableText){
+            if(character == '\n'){
+                string.add(line);
+                line = new ArrayList<>();
+            } else {
+                line.add(character);
+                numCharacters++;
+            }
+        }
+        float[] positions = new float[numCharacters*12];
+        float[] textureCoordinates = new float[numCharacters*8];
+        int[] indices = new int[numCharacters*6];
+        string.add(line);
+        float YStart = centerY ? -string.size()/2f : 0; //the farthest up coordinate of the text.
+
+        int charIndex = 0;
+        for(int i=0; i < string.size(); i++){
+            float XStart = centerX ? -string.get(i).size()/2f : 0; //the farthest left coordinate of the text.
+            for(int j=0; j<string.get(i).size(); j++){
+                char character = string.get(i).get(j);
+                int column = character & 15;
+                int row = character >> 4 & 15; //get the last 4 bits and first 4 bits
+
+                //UV coordinates
+                float UVXPosition = column*0.0625f;
+                float UVYPosition = row*0.0625f; //get the actual UV coordinates of the top left corner
+                int j8 = 8*charIndex;
+                textureCoordinates[j8  ] = UVXPosition;
+                textureCoordinates[j8+1] = UVYPosition; //set the top left
+
+                textureCoordinates[j8+2] = UVXPosition+0.0625f;
+                textureCoordinates[j8+3] = UVYPosition; //top-right
+
+                textureCoordinates[j8+4] = UVXPosition;
+                textureCoordinates[j8+5] = UVYPosition + 0.0625f; //bottom left
+
+                textureCoordinates[j8+6] = UVXPosition + 0.0625f;
+                textureCoordinates[j8+7] = UVYPosition + 0.0625f; //bottom right
+
+                //vertex positions
+                int j12 = charIndex*12;
+                float iXStart = XStart+j;
+                float iYStart = YStart-i;
+                positions[j12  ] = iXStart; //top left
+                positions[j12+1] = iYStart+1;
+                positions[j12+2] = 0;
+
+                positions[j12+3] = iXStart+1; //top right
+                positions[j12+4] = iYStart+1;
+                positions[j12+5] = 0;
+
+                positions[j12+6] = iXStart; //bottom left
+                positions[j12+7] = iYStart;
+                positions[j12+8] = 0;
+
+                positions[j12+9] = iXStart+1; //bottom right
+                positions[j12+10] = iYStart;
+                positions[j12+11] = 0;
+
+                //indices 0, 1, 2, 1, 2, 3
+                int j6 = charIndex*6;
+                indices[j6  ] = charIndex*4;
+                indices[j6+1] = 1+charIndex*4;
+                indices[j6+2] = 2+charIndex*4;
+
+                indices[j6+3] = 1+charIndex*4;
+                indices[j6+4] = 2+charIndex*4;
+                indices[j6+5] = 3+charIndex*4;
+                charIndex++;
+            }
+        }
+
+        return new CPUMesh(positions, textureCoordinates, indices);
     }
 
 }
